6d19d0bf23ce1aa53050ff4a7cec7782ad4909e2
ctdupuis <ctdupuis7@gmail.com>
2020-04-26 13:30:52 -0500

    started messing with fetches

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,20 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_PATH = 'http://localhost:3000'
+const ALBUMS_PATH = `${BASE_PATH}/albums`
+
+fetch(ALBUMS_PATH)
+.then(res => res.json())
+.then(json => json.forEach(album => renderAlbum(album)))
+
+function renderAlbum(album){
+    debugger
+    let flexContainer = document.querySelector('div.flex-container')
+    let divCard = document.createElement('div')
+    divCard.setAttribute('data-album-id', album.id)
+    let html = `
+    <div class="img-container">
+        <img src="${album.img_url}">
+    </div>
+    `
+    divCard.innerHTML += html
+    flexContainer.appendChild(divCard)
+}
