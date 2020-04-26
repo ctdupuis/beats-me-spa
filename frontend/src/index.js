50f3941b36a0f19db3fa3279a3ce625c26dae3d5
ctdupuis <ctdupuis7@gmail.com>
2020-04-26 14:07:04 -0500

    flex-container, minor CSS for visual

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,25 @@
 const BASE_PATH = 'http://localhost:3000'
 const ALBUMS_PATH = `${BASE_PATH}/albums`
 
+document.querySelector('form#new-album').addEventListener("submit", function(e){
+    debugger
+    e.preventDefault();
+})
+
 fetch(ALBUMS_PATH)
 .then(res => res.json())
 .then(json => json.forEach(album => renderAlbum(album)))
 
 function renderAlbum(album){
-    debugger
+    // debugger
     let flexContainer = document.querySelector('div.flex-container')
     let divCard = document.createElement('div')
     divCard.setAttribute('data-album-id', album.id)
+    divCard.setAttribute('class', 'album-card')
     let html = `
     <div class="img-container">
+    <span class="alb-name">${album.name}</span>
+
         <img src="${album.img_url}">
     </div>
     `
