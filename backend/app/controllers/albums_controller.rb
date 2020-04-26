aa9a28ff9e9eb291c18ca90a6d309dc43a522290
ctdupuis <ctdupuis7@gmail.com>
2020-04-26 11:08:35 -0500

    start album controller

--- backend/app/controllers/albums_controller.rb
+++ backend/app/controllers/albums_controller.rb
@@ -1,2 +1,25 @@
 class AlbumsController < ApplicationController
+    before_action :set_album, except: [:new, :create, :index]
+
+    def new 
+
+    end
+
+    def create
+
+    end
+
+    def index
+        albums = Album.all
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:name, :artist_name, :img_url, :genre_id, songs_attributes: [:title, :runtime, :artist_name])
+    end
+
+    def set_album
+        album = Album.find(params[:id])
+    end
 end
