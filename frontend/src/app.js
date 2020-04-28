4535807bd8770192f94dae0126134dd4db32385d
ctdupuis <ctdupuis7@gmail.com>
2020-04-27 22:26:52 -0500

    albums render, move to tracklist render

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -20,28 +20,31 @@ class App {
         .then(json =>  {
             json.forEach(album => {
                 let alb = new Album(album.name, album.artist.name, album.genre.name, album.img_url, album.songs)
+                debugger
                 this.albums.push(alb)
                 debugger
+            })
+            this.albums.forEach(album => {
+                this.renderAlbum(album)
+                this.renderSongs(album)
             })
         }
         )
         .catch(err => alert(err))
     }
 
-    renderAlbums = () => {
-        this.albums.forEach(album => {
-            let html = `<span class="alb-name">${album.name}</span>
-            <div class="img-container">
-                <img src="${album.img_url}">
-            </div>
-            <span class="alb-artist">${album.artist.name}</span>
+    renderAlbum = (album) => {
+        let html = `<span class="alb-name">${album.name}</span>
+        <div class="img-container">
+            <img src="${album.imgURL}">
+        </div>
+        <span class="alb-artist">${album.artist}</span>
             `
-            let divCard = document.createElement('div')
-            divCard.setAttribute('data-album-id', album.id)
-            divCard.setAttribute('class', 'album-card')
-            divCard.innerHTML += html
-            this.flexContainer.appendChild(divCard)
-        })
+        let divCard = document.createElement('div')
+        divCard.setAttribute('data-album-id', album.id)
+        divCard.setAttribute('class', 'album-card')
+        divCard.innerHTML += html
+        this.flexContainer.appendChild(divCard)
     }
 }
 
