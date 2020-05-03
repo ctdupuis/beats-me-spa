6e4cbe535835f35643e518f70d620371d255e96d
ctdupuis <ctdupuis7@gmail.com>
2020-05-02 22:14:30 -0500

    move data setup from app to fetch class

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,5 @@
 class App {
     constructor(){
-        // this.go = new Fetch
         // this.baseURL = baseURL
         this.albumsURL = `http://localhost:3000/albums`
         this.newAlbumForm = document.querySelector('form#new-album')
@@ -12,7 +11,6 @@ class App {
         this._session = {}
         this.newAlbumForm.addEventListener("submit", this.postData)
         this.formBtn.addEventListener("click", this.displayForm)
-        // document.addEventListener("DOMContentLoaded" )
        
     }
 
@@ -61,8 +59,8 @@ class App {
     }
 
     postData = (e) => {
-        let albumdata = this.setupAlbObj(e)
-        new Fetch(this.albumsURL, 'POST', albumdata).post()
+        // let albumdata = this.setupAlbObj(e)
+        new Fetch(this.albumsURL, 'POST', e).post()
         .then(alb => {
             let album = new Album(alb.id, alb.name, alb.artist.name, alb.genre.name, alb.img_url, alb.songs)
             this.makeCard(this.flexContainer, album) 
@@ -70,35 +68,6 @@ class App {
         })
         e.preventDefault();
         this.newAlbumForm.style.display = "none"
-    }
-
-    setupAlbObj = (event) => {
-        let data = {
-            album: { 
-                name: event.target.children["album-name"].value,
-                artist_name: event.target.children["artist-name"].value,
-                genre_name: event.target.children['genre'].value,
-                img_url: event.target.children['album-img'].value,
-                songs_attributes: []
-            }
-        }
-        // form an array of input fields
-        let songs = Array.from(event.target.children).filter(child => {
-            return child.className === 'track-input'
-        })
-        // loop to format the field values for submission
-        let x = 0
-        while (songs[x] !== undefined) {
-            let songObj = Object.assign({}, {title: songs[x].value, runtime: songs[x+1].value});
-            if (songObj.title === "" && songObj.runtime === "") {
-                break;
-            } else {
-            debugger
-            data.album.songs_attributes.push(songObj)
-            x += 2
-            }
-        }
-        return data
     }
 
     addListeners = () => {
@@ -126,7 +95,7 @@ class App {
         delBtns.forEach(btn => {
             btn.addEventListener('click', (e) => {
                 new Fetch(this.albumsURL, 'DELETE').delete(e)
-                alert("Success!")
+                alert("Album Deleted")
                 e.target.parentElement.remove()
             })
         }) 
--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -1,20 +1,19 @@
 class Fetch {
-    constructor(url, method=null, data=null) {
+    constructor(url, method=null, event) {
         this.url = url
         this.headerObj = {
             "Content-Type": "application/json",
             "accept": "application/json"
         }
         this.method = method
-        this.data = data
         this.configObject = {
             method: method,
             headers: {
                 "Content-Type": "application/json",
                 "accept": "application/json"  
-            },
-            body: JSON.stringify(data)
+            }
         }
+        this.event = event
     }
 
     get = function() {
@@ -22,7 +21,37 @@ class Fetch {
         .then(r => r.json())
     }
 
+    setupAlbObj = (event) => {
+        let data = {
+            album: { 
+                name: event.target.children["album-name"].value,
+                artist_name: event.target.children["artist-name"].value,
+                genre_name: event.target.children['genre'].value,
+                img_url: event.target.children['album-img'].value,
+                songs_attributes: []
+            }
+        }
+        // form an array of input fields
+        let songs = Array.from(event.target.children).filter(child => {
+            return child.className === 'track-input'
+        })
+        // loop to format the field values for submission
+        let x = 0
+        while (songs[x] !== undefined) {
+            let songObj = Object.assign({}, {title: songs[x].value, runtime: songs[x+1].value});
+            if (songObj.title === "" && songObj.runtime === "") {
+                break;
+            } else {
+            data.album.songs_attributes.push(songObj)
+            x += 2
+            }
+        }
+        return data
+    }
+
     post = function() {
+        let albumdata = this.setupAlbObj(this.event)
+        this.configObject.body = JSON.stringify(albumdata)
         return fetch(this.url, this.configObject)
         .then(r => r.json())
     }
@@ -33,7 +62,15 @@ class Fetch {
         .then(r => r.json())
     }
 
-    static destroy(url) {
+    signup = function(event) {
+
+    }
+
+    login = function(event) {
+
+    }
+
+    logout = function(event) {
 
     }
 }
