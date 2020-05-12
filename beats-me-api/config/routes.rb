bdbfabe8586568ef0f3aff01a943f34ac8d6d441
ctdupuis <ctdupuis7@gmail.com>
2020-05-11 21:22:01 -0500

    last minute cleanup

--- beats-me-api/config/routes.rb
+++ beats-me-api/config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   resources :albums do 
     resources :songs
   end
+  resources :artists
 
   get '/songs' => 'songs#index'
   get '/genres' => 'genres#index'
-  post '/signup' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logged_in' => 'sessions#logged_in'
-  get '/logout' => 'sessions#logout'
-  resources :artists
+  # post '/signup' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # get '/logged_in' => 'sessions#logged_in'
+  # get '/logout' => 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- frontend/src/index.js
+++
@@ -1,5 +0,0 @@
-const path = 'http://localhost:3000/albums'
-// let go = new Fetch
-let app = new App
-app.start()
-app.addListeners()
